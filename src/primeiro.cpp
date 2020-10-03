553083f9da93dce166afc1da789305816b04cea0
Lucas <lucsousan@gmail.com>
2020-10-02 22:03:23 -0300

    Update primeiro.cpp

---
+++ src/primeiro.cpp
@@ -0,0 +1,10 @@
+/*
+1) Escreva um programa que receba como entradas um número primo p < 100 e um número a ∈ Zp, e
+forneça em sua saída uma raiz quadrada de a (mod p), isto é, um número b ∈ Zp, tal que b
+
+2 ≡ a (mod p).
+Depois, responda às perguntas a seguir (justifique todas as suas respostas com base nas simulações que
+realizar e à luz de resultados associados aos chamados “resíduos quadráticos” módulo p):
+a) É sempre que a raiz quadrada em questão existe? Explique.
+b) Quando a raiz quadrada em questão existe, ela é única? Justifique.
+*/
